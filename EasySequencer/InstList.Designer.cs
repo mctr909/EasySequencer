b5b76009c65c0a12e7ddff19365984fc3d1082ac
松崎好浩 <y.matsuzaki@ris.co.jp>
2019-03-21 10:41:50 +0900

    追加

---
+++ EasySequencer/InstList.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace EasySequencer {
+    partial class InstList {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lstInst = new System.Windows.Forms.ListBox();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.btnCommit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstInst
+            // 
+            this.lstInst.ColumnWidth = 150;
+            this.lstInst.Font = new System.Drawing.Font("MS UI Gothic", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lstInst.FormattingEnabled = true;
+            this.lstInst.HorizontalExtent = 1;
+            this.lstInst.HorizontalScrollbar = true;
+            this.lstInst.ItemHeight = 37;
+            this.lstInst.Location = new System.Drawing.Point(12, 64);
+            this.lstInst.MultiColumn = true;
+            this.lstInst.Name = "lstInst";
+            this.lstInst.ScrollAlwaysVisible = true;
+            this.lstInst.Size = new System.Drawing.Size(831, 411);
+            this.lstInst.TabIndex = 1;
+            this.lstInst.SelectedIndexChanged += new System.EventHandler(this.lstInst_SelectedIndexChanged);
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.Font = new System.Drawing.Font("MS UI Gothic", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(13, 13);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(393, 45);
+            this.cmbCategory.TabIndex = 0;
+            this.cmbCategory.SelectedIndexChanged += new System.EventHandler(this.cmbCategory_SelectedIndexChanged);
+            // 
+            // btnCommit
+            // 
+            this.btnCommit.Location = new System.Drawing.Point(699, 13);
+            this.btnCommit.Name = "btnCommit";
+            this.btnCommit.Size = new System.Drawing.Size(144, 45);
+            this.btnCommit.TabIndex = 2;
+            this.btnCommit.Text = "確定";
+            this.btnCommit.UseVisualStyleBackColor = true;
+            this.btnCommit.Click += new System.EventHandler(this.btnCommit_Click);
+            // 
+            // InstList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 484);
+            this.Controls.Add(this.btnCommit);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.lstInst);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "InstList";
+            this.Text = "InstList";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstInst;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Button btnCommit;
+    }
+}
---
+++ EasySequencer/InstList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+using MIDI;
+
+namespace EasySequencer {
+    public partial class InstList : Form {
+        private Channel mChannel;
+        private Dictionary<string , Dictionary<INST_ID, string>> mInstList;
+
+        public InstList(Channel channel) {
+            InitializeComponent();
+            mInstList = new Dictionary<string, Dictionary<INST_ID, string>>();
+            mChannel = channel;
+
+            var selectedCategory = "";
+            var selectedInst = 0;
+
+            foreach (var inst in mChannel.InstList) {
+                var cat = inst.Value[1];
+                var nam = inst.Value[0];
+                if (!mInstList.ContainsKey(cat)) {
+                    mInstList.Add(cat, new Dictionary<INST_ID, string>());
+                    cmbCategory.Items.Add(cat);
+                }
+
+                mInstList[cat].Add(inst.Key, nam);
+                if (mChannel.InstId.isDrum == inst.Key.isDrum
+                    && mChannel.InstId.programNo == inst.Key.programNo
+                    && mChannel.InstId.bankMSB == inst.Key.bankMSB
+                    && mChannel.InstId.bankLSB == inst.Key.bankLSB) {
+                    selectedCategory = cat;
+                    selectedInst = mInstList[cat].Count - 1;
+                }
+            }
+
+            cmbCategory.SelectedItem = selectedCategory;
+            lstInst.Items.Clear();
+            foreach (var inst in mInstList[selectedCategory]) {
+                lstInst.Items.Add(inst.Value);
+            }
+            lstInst.SelectedIndex = selectedInst;
+        }
+
+        private void cmbCategory_SelectedIndexChanged(object sender, EventArgs e) {
+            if(!mInstList.ContainsKey((string)cmbCategory.SelectedItem)) {
+                return;
+            }
+
+            lstInst.Items.Clear();
+            foreach (var inst in mInstList[(string)cmbCategory.SelectedItem]) {
+                lstInst.Items.Add(inst.Value);
+            }
+        }
+
+        private void lstInst_SelectedIndexChanged(object sender, EventArgs e) {
+            if (!mInstList.ContainsKey((string)cmbCategory.SelectedItem)) {
+                return;
+            }
+            var list = mInstList[(string)cmbCategory.SelectedItem].ToArray();
+            var inst = list[(lstInst.SelectedIndex < list.Count()) ? lstInst.SelectedIndex : list.Count() - 1];
+
+            mChannel.CtrlChange(CTRL_TYPE.BANK_MSB, inst.Key.bankMSB);
+            mChannel.CtrlChange(CTRL_TYPE.BANK_LSB, inst.Key.bankLSB);
+            mChannel.ProgramChange(inst.Key.programNo, inst.Key.isDrum == 0x80);
+        }
+
+        private void btnCommit_Click(object sender, EventArgs e) {
+            Close();
+        }
+    }
+}
