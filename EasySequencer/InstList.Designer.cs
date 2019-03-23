5a844c539ea5bda5d2a117b32203b4925be2c460
松崎好浩 <y.matsuzaki@ris.co.jp>
2019-03-23 10:32:14 +0900

    更新

--- EasySequencer/InstList.Designer.cs
+++ EasySequencer/InstList.Designer.cs
@@ -31,16 +31,16 @@
             // lstInst
             // 
             this.lstInst.ColumnWidth = 150;
-            this.lstInst.Font = new System.Drawing.Font("MS UI Gothic", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lstInst.Font = new System.Drawing.Font("MS UI Gothic", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.lstInst.FormattingEnabled = true;
             this.lstInst.HorizontalExtent = 1;
             this.lstInst.HorizontalScrollbar = true;
-            this.lstInst.ItemHeight = 37;
+            this.lstInst.ItemHeight = 29;
             this.lstInst.Location = new System.Drawing.Point(12, 64);
             this.lstInst.MultiColumn = true;
             this.lstInst.Name = "lstInst";
             this.lstInst.ScrollAlwaysVisible = true;
-            this.lstInst.Size = new System.Drawing.Size(831, 411);
+            this.lstInst.Size = new System.Drawing.Size(1071, 381);
             this.lstInst.TabIndex = 1;
             this.lstInst.SelectedIndexChanged += new System.EventHandler(this.lstInst_SelectedIndexChanged);
             // 
@@ -50,15 +50,16 @@
             this.cmbCategory.FormattingEnabled = true;
             this.cmbCategory.Location = new System.Drawing.Point(13, 13);
             this.cmbCategory.Name = "cmbCategory";
-            this.cmbCategory.Size = new System.Drawing.Size(393, 45);
+            this.cmbCategory.Size = new System.Drawing.Size(516, 45);
             this.cmbCategory.TabIndex = 0;
             this.cmbCategory.SelectedIndexChanged += new System.EventHandler(this.cmbCategory_SelectedIndexChanged);
             // 
             // btnCommit
             // 
-            this.btnCommit.Location = new System.Drawing.Point(699, 13);
+            this.btnCommit.Font = new System.Drawing.Font("MS UI Gothic", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnCommit.Location = new System.Drawing.Point(939, 10);
             this.btnCommit.Name = "btnCommit";
-            this.btnCommit.Size = new System.Drawing.Size(144, 45);
+            this.btnCommit.Size = new System.Drawing.Size(144, 48);
             this.btnCommit.TabIndex = 2;
             this.btnCommit.Text = "確定";
             this.btnCommit.UseVisualStyleBackColor = true;
@@ -68,7 +69,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(853, 484);
+            this.ClientSize = new System.Drawing.Size(1095, 458);
             this.Controls.Add(this.btnCommit);
             this.Controls.Add(this.cmbCategory);
             this.Controls.Add(this.lstInst);
