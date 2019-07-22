b17817f3cec69bd63b0d2db786b16f25d1593471
mctr909 <tr-909@hotmail.co.jp>
2019-07-22 16:31:58 +0900

    Add files via upload

--- EasySequencer/Midi/Channel.cs
+++ EasySequencer/Midi/Channel.cs
@@ -3,9 +3,10 @@
 namespace MIDI {
     unsafe public class Channel {
         private CHANNEL_PARAM* mpChannel = null;
-        private Instruments mInst = null;
+        private Instruments mInstruments = null;
         private INST_ID mInstId;
-        private CONTROL ctrl;
+
+        public Dictionary<INST_ID, string[]> InstList { get { return mInstruments.Names; } }
 
         public bool Enable;
 
@@ -13,58 +14,53 @@ namespace MIDI {
 
         public INST_ID InstId { get { return mInstId; } }
 
-        public KEY_STATUS[] KeyBoard { get; set; }
+        public string InstName { get; private set; }
+
+        public KEY_STATUS[] KeyBoard { get; private set; }
 
         public WAVE_INFO[] WaveInfo { get; private set; }
 
-        public string InstName { get; private set; }
+        public byte Vol { get; private set; }
 
-        public Dictionary<INST_ID, string[]> InstList { get { return mInst.Names; } }
+        public byte Exp { get; private set; }
 
-        public byte Vol {
-            get { return ctrl.vol; }
-        }
+        public byte Pan { get; private set; }
 
-        public byte Exp {
-            get { return ctrl.exp; }
-        }
+        public byte Rev { get; private set; }
 
-        public byte Pan {
-            get { return ctrl.pan; }
-        }
+        public byte Del { get; private set; }
 
-        public byte Rev {
-            get { return ctrl.rev; }
-        }
+        public byte Cho { get; private set; }
 
-        public byte Del {
-            get { return ctrl.del; }
-        }
+        public byte Mod { get; private set; }
 
-        public byte Cho {
-            get { return ctrl.cho; }
-        }
+        public byte Hld { get; private set; }
 
-        public byte Hld {
-            get { return ctrl.hold; }
-        }
+        public byte Fc { get; private set; }
 
-        public byte Fc {
-            get { return ctrl.cut; }
-        }
+        public byte Fq { get; private set; }
 
-        public byte Fq {
-            get { return ctrl.res; }
-        }
+        public byte Atk { get; private set; }
 
-        public byte BendRange {
-            get { return ctrl.bendRange; }
-        }
+        public byte Rel { get; private set; }
+
+        public byte VibRate { get; private set; }
+
+        public byte VibDepth { get; private set; }
+
+        public byte VibDelay { get; private set; }
+
+        public byte BendRange { get; private set; }
 
         public int Pitch { get; private set; }
 
+        private byte mRpnLSB;
+        private byte mRpnMSB;
+        private byte mNrpnLSB;
+        private byte mNrpnMSB;
+
         public Channel(Instruments inst, CHANNEL_PARAM* pChannel, int no) {
-            mInst = inst;
+            mInstruments = inst;
             mpChannel = pChannel;
             No = (byte)no;
             KeyBoard = new KEY_STATUS[128];
@@ -79,35 +75,36 @@ namespace MIDI {
             setAmp(100, 100);
             setPan(64);
 
-            setHold(0);
+            setHld(0);
+
+            Rev = 0;
+            setCho(0);
+            setDel(0);
 
             setRes(64);
             setCut(64);
 
-            ctrl.rel = 64;
-            ctrl.atk = 64;
+            Rel = 64;
+            Atk = 64;
 
-            ctrl.vibRate = 64;
-            ctrl.vibDepth = 64;
-            ctrl.vibDelay = 64;
-
-            ctrl.rev = 0;
-            setChorusDepth(0);
-            setDelayDepath(0);
+            VibRate = 64;
+            VibDepth = 64;
+            VibDelay = 64;
 
             mpChannel->chorusRate = 0.01;
             mpChannel->delayTime = 0.2;
 
-            ctrl.nrpnLSB = 0xFF;
-            ctrl.nrpnMSB = 0xFF;
-            ctrl.rpnLSB = 0xFF;
-            ctrl.rpnMSB = 0xFF;
+            mRpnLSB = 0xFF;
+            mRpnMSB = 0xFF;
+            mNrpnLSB = 0xFF;
+            mNrpnMSB = 0xFF;
 
-            ctrl.bendRange = 2;
-
-            mpChannel->pitch = 1.0;
+            BendRange = 2;
             Pitch = 0;
+            mpChannel->pitch = 1.0;
 
+            mInstId.bankMSB = 0;
+            mInstId.bankLSB = 0;
             ProgramChange(0);
         }
 
@@ -120,22 +117,28 @@ namespace MIDI {
                     mInstId.bankLSB = b1;
                     break;
 
-                case CTRL_TYPE.DATA:
-                    rpn(b1);
-                    break;
-
-                case CTRL_TYPE.EXPRESSION:
-                    setAmp(ctrl.vol, b1);
+                case CTRL_TYPE.VOLUME:
+                    setAmp(b1, Exp);
                     break;
                 case CTRL_TYPE.PAN:
                     setPan(b1);
                     break;
-                case CTRL_TYPE.VOLUME:
-                    setAmp(b1, ctrl.exp);
+                case CTRL_TYPE.EXPRESSION:
+                    setAmp(Vol, b1);
+                    break;
+
+                case CTRL_TYPE.MODULATION:
+                    Mod = b1;
                     break;
 
                 case CTRL_TYPE.HOLD:
-                    setHold(b1);
+                    setHld(b1);
+                    break;
+                case CTRL_TYPE.RELEACE:
+                    Rel = b1;
+                    break;
+                case CTRL_TYPE.ATTACK:
+                    Atk = b1;
                     break;
 
                 case CTRL_TYPE.RESONANCE:
@@ -145,38 +148,41 @@ namespace MIDI {
                     setCut(b1);
                     break;
 
-                case CTRL_TYPE.RELEACE:
-                    ctrl.rel = b1;
-                    break;
-                case CTRL_TYPE.ATTACK:
-                    ctrl.atk = b1;
-                    break;
-
                 case CTRL_TYPE.VIB_RATE:
-                    ctrl.vibRate = b1;
+                    VibRate = b1;
                     break;
                 case CTRL_TYPE.VIB_DEPTH:
-                    ctrl.vibDepth = b1;
+                    VibDepth = b1;
                     break;
                 case CTRL_TYPE.VIB_DELAY:
-                    ctrl.vibDelay = b1;
+                    VibDelay = b1;
                     break;
 
                 case CTRL_TYPE.REVERB:
-                    ctrl.rev = b1;
+                    Rev = b1;
                     break;
                 case CTRL_TYPE.CHORUS:
-                    setChorusDepth(b1);
+                    setCho(b1);
                     break;
                 case CTRL_TYPE.DELAY:
-                    setDelayDepath(b1);
+                    setDel(b1);
                     break;
 
+                case CTRL_TYPE.NRPN_LSB:
+                    mNrpnLSB = b1;
+                    break;
+                case CTRL_TYPE.NRPN_MSB:
+                    mNrpnMSB = b1;
+                    break;
                 case CTRL_TYPE.RPN_LSB:
-                    ctrl.rpnLSB = b1;
+                    mRpnLSB = b1;
                     break;
                 case CTRL_TYPE.RPN_MSB:
-                    ctrl.rpnMSB = b1;
+                    mRpnMSB = b1;
+                    break;
+                case CTRL_TYPE.DATA:
+                    setRpn(b1);
+                    setNrpn(b1);
                     break;
 
                 case CTRL_TYPE.ALL_RESET:
@@ -189,38 +195,38 @@ namespace MIDI {
             mInstId.isDrum = (byte)(9 == No ? 0x80 : 0x00);
             mInstId.programNo = value;
 
-            if (!mInst.List.ContainsKey(mInstId)) {
+            if (!mInstruments.List.ContainsKey(mInstId)) {
                 mInstId.bankMSB = 0;
                 mInstId.bankLSB = 0;
-                if (!mInst.List.ContainsKey(mInstId)) {
+                if (!mInstruments.List.ContainsKey(mInstId)) {
                     mInstId.programNo = 0;
                 }
             }
 
-            WaveInfo = mInst.List[mInstId];
-            InstName = mInst.Names[mInstId][0];
+            WaveInfo = mInstruments.List[mInstId];
+            InstName = mInstruments.Names[mInstId][0];
         }
 
         public void ProgramChange(byte value, bool isDrum) {
             mInstId.isDrum = (byte)(isDrum ? 0x80 : 0x00);
             mInstId.programNo = value;
 
-            if (!mInst.List.ContainsKey(mInstId)) {
+            if (!mInstruments.List.ContainsKey(mInstId)) {
                 mInstId.bankMSB = 0;
                 mInstId.bankLSB = 0;
-                if (!mInst.List.ContainsKey(mInstId)) {
+                if (!mInstruments.List.ContainsKey(mInstId)) {
                     mInstId.programNo = 0;
                 }
             }
 
-            WaveInfo = mInst.List[mInstId];
-            InstName = mInst.Names[mInstId][0];
+            WaveInfo = mInstruments.List[mInstId];
+            InstName = mInstruments.Names[mInstId][0];
         }
 
         public void PitchBend(byte lsb, byte msb) {
             Pitch = (lsb | (msb << 7)) - 8192;
 
-            var temp = Pitch * ctrl.bendRange;
+            var temp = Pitch * BendRange;
             if (temp < 0) {
                 temp = -temp;
                 mpChannel->pitch = 1.0 / (Const.SemiTone[temp >> 13] * Const.PitchMSB[(temp >> 7) % 64] * Const.PitchLSB[temp % 128]);
@@ -232,18 +238,18 @@ namespace MIDI {
 
         /******************************************************************************/
         private void setAmp(byte vol, byte exp) {
-            ctrl.vol = vol;
-            ctrl.exp = exp;
+            Vol = vol;
+            Exp = exp;
             mpChannel->amp = Const.Amp[vol] * Const.Amp[exp];
         }
 
         private void setPan(byte value) {
-            ctrl.pan = value;
+            Pan = value;
             mpChannel->panLeft = Const.Cos[value];
             mpChannel->panRight = Const.Sin[value];
         }
 
-        private void setHold(byte value) {
+        private void setHld(byte value) {
             if (value < 64) {
                 for (byte k = 0; k < 128; ++k) {
                     if (KEY_STATUS.HOLD == KeyBoard[k]) {
@@ -251,42 +257,49 @@ namespace MIDI {
                     }
                 }
             }
-
-            ctrl.hold = value;
+            Hld = value;
             mpChannel->holdDelta = (value < 64 ? 1.0 : 1.0 * Const.DeltaTime);
         }
 
         private void setRes(byte value) {
-            ctrl.res = value;
+            Fq = value;
             mpChannel->resonance = (value < 64) ? 0.0 : ((value - 64) / 64.0);
         }
 
         private void setCut(byte value) {
-            ctrl.cut = value;
+            Fc = value;
             mpChannel->cutoff = (value < 64) ? Const.Level[(int)(2.0 * value)] : 1.0;
         }
 
-        private void setDelayDepath(byte value) {
-            ctrl.del = value;
+        private void setDel(byte value) {
+            Del = value;
             mpChannel->delayDepth = 0.8 * Const.FeedBack[value];
         }
 
-        private void setChorusDepth(byte value) {
-            ctrl.cho = value;
+        private void setCho(byte value) {
+            Cho = value;
             mpChannel->chorusDepth = 2.0 * Const.FeedBack[value];
         }
 
-        private void rpn(byte b1) {
-            switch (ctrl.rpnLSB | ctrl.rpnMSB << 8) {
+        private void setRpn(byte b1) {
+            switch (mRpnLSB | mRpnMSB << 8) {
                 case 0x0000:
-                    ctrl.bendRange = b1;
+                    BendRange = b1;
                     break;
                 default:
                     break;
             }
+            mRpnMSB = 0xFF;
+            mRpnLSB = 0xFF;
+        }
 
-            ctrl.rpnMSB = 0xFF;
-            ctrl.rpnLSB = 0xFF;
+        private void setNrpn(byte b1) {
+            switch (mNrpnLSB | mNrpnMSB << 8) {
+                default:
+                    break;
+            }
+            mNrpnMSB = 0xFF;
+            mNrpnLSB = 0xFF;
         }
     }
 }
