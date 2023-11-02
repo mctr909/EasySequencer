2b9b6bf7bf98c07c147f74d42f0ed88b3dc762df
松崎好浩 <y.matsuzaki@ris.co.jp>
2023-11-02 17:51:05 +0900

    更新

--- EasySequencer/ChordHelper.cs
+++ EasySequencer/ChordHelper.cs
@@ -2,107 +2,105 @@
 
 namespace EasySequencer {
     class ChordHelper {
-        enum I {
+        struct I {
             /// <summary>完全1度</summary>
-            P1 = 0x0,
+            public const byte P1 = 0x0;
             /// <summary>長2度</summary>
-            M2 = 0x2,
+            public const byte M2 = 0x2;
             /// <summary>長3度</summary>
-            M3 = 0x4,
+            public const byte M3 = 0x4;
             /// <summary>完全4度</summary>
-            P4 = 0x5,
+            public const byte P4 = 0x5;
             /// <summary>完全5度</summary>
-            P5 = 0x7,
+            public const byte P5 = 0x7;
             /// <summary>長6度</summary>
-            M6 = 0x9,
+            public const byte M6 = 0x9;
             /// <summary>長7度</summary>
-            M7 = 0xB,
+            public const byte M7 = 0xB;
             /// <summary>短音程</summary>
-            MIN = 0x10,
+            public const byte MIN = 0x10;
             /// <summary>短2度</summary>
-            m2 = MIN | M2,
+            public const byte m2 = MIN | M2;
             /// <summary>短3度</summary>
-            m3 = MIN | M3,
+            public const byte m3 = MIN | M3;
             /// <summary>短6度</summary>
-            m6 = MIN | M6,
+            public const byte m6 = MIN | M6;
             /// <summary>短7度</summary>
-            m7 = MIN | M7,
+            public const byte m7 = MIN | M7;
             /// <summary>減音程</summary>
-            DIM = 0x20,
+            public const byte DIM = 0x20;
             /// <summary>減1度</summary>
-            b1 = DIM | P1,
+            public const byte b1 = DIM | P1;
             /// <summary>減2度</summary>
-            b2 = DIM | m2,
+            public const byte b2 = DIM | m2;
             /// <summary>減3度</summary>
-            b3 = DIM | m3,
+            public const byte b3 = DIM | m3;
             /// <summary>減4度</summary>
-            b4 = DIM | P4,
+            public const byte b4 = DIM | P4;
             /// <summary>減5度</summary>
-            b5 = DIM | P5,
+            public const byte b5 = DIM | P5;
             /// <summary>減6度</summary>
-            b6 = DIM | m6,
+            public const byte b6 = DIM | m6;
             /// <summary>減7度</summary>
-            b7 = DIM | m7,
+            public const byte b7 = DIM | m7;
             /// <summary>増音程</summary>
-            AUG = 0x40,
+            public const byte AUG = 0x40;
             /// <summary>増1度</summary>
-            s1 = AUG | P1,
+            public const byte s1 = AUG | P1;
             /// <summary>増2度</summary>
-            s2 = AUG | M2,
+            public const byte s2 = AUG | M2;
             /// <summary>増3度</summary>
-            s3 = AUG | M3,
+            public const byte s3 = AUG | M3;
             /// <summary>増4度</summary>
-            s4 = AUG | P4,
+            public const byte s4 = AUG | P4;
             /// <summary>増5度</summary>
-            s5 = AUG | P5,
+            public const byte s5 = AUG | P5;
             /// <summary>増6度</summary>
-            s6 = AUG | M6,
+            public const byte s6 = AUG | M6;
             /// <summary>増7度</summary>
-            s7 = AUG | M7,
+            public const byte s7 = AUG | M7;
             /// <summary>テンション</summary>
-            T = 0x80,
+            public const byte T = 0x80;
             /// <summary>短9度</summary>
-            m9 = T | m2,
+            public const byte m9 = T | m2;
             /// <summary>長9度</summary>
-            M9 = T | M2,
+            public const byte M9 = T | M2;
             /// <summary>完全11度</summary>
-            P11 = T | P4,
+            public const byte P11 = T | P4;
             /// <summary>増11度</summary>
-            s11 = T | s4,
+            public const byte s11 = T | s4;
             /// <summary>短13度</summary>
-            m13 = T | m6,
+            public const byte m13 = T | m6;
             /// <summary>長13度</summary>
-            M13 = T | M6
-        }
-        struct Interval {
-            public I Id;
-            public int Tone;
-            public Interval(I id) {
+            public const byte M13 = T | M6;
+
+            public readonly byte Id;
+            public readonly int Tone;
+            public I(byte id) {
                 Id = id;
-                var v = (int)id;
-                Tone = v & 0xF;
-                if (0 < (v & (int)I.MIN)) {
+                Tone = id & 0xF;
+                if (0 < (id & MIN)) {
                     Tone--;
                 }
-                if (0 < (v & (int)I.DIM)) {
+                if (0 < (id & DIM)) {
                     Tone--;
                 }
-                if (0 < (v & (int)I.AUG)) {
+                if (0 < (id & AUG)) {
                     Tone++;
                 }
             }
         }
         struct Structure {
-            public Interval[] Intervals;
+            public I[] Intervals;
             public string[] Names;
-            Structure(Interval[] intervals, params string[] names) {
+            Structure(I[] intervals, params string[] names) {
                 Intervals = intervals;
                 Names = names;
             }
-            static Interval[] ToArray(params I[] structure) {
-                var list = new List<Interval>();
-                for (int i = 0; i < structure.Length; i++) {
-                    list.Add(new Interval(structure[i]));
+            static I[] ToArray(params byte[] intervals) {
+                var list = new List<I>();
+                for (int i = 0; i < intervals.Length; i++) {
+                    list.Add(new I(intervals[i]));
                 }
                 list.Sort((a, b) => { return a.Tone - b.Tone; });
                 return list.ToArray();
@@ -235,8 +233,8 @@ namespace EasySequencer {
                                 }
                                 break;
                             }
-                            var v = (int)interval.Id;
-                            var flat = 1 != root.Tone.IndexOf("#") && (1 == root.Tone.IndexOf("b") || 0 < (v & (int)I.MIN) || 0 < (v & (int)I.DIM));
+                            var v = interval.Id;
+                            var flat = 1 != root.Tone.IndexOf("#") && (1 == root.Tone.IndexOf("b") || 0 < (v & I.MIN) || 0 < (v & I.DIM));
                             var bass = SCALE.GetName(bassTone, flat);
                             return new string[] { root.Degree, root.Tone, structureName + " on " + bass.Tone };
                         }
